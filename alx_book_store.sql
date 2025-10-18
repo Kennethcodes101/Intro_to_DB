38fee96b9de718d577f6d4f36c81a80a5e2efc96
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-10-18 20:25:40 +0300

    First commit for database connection

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    author_id REFERENCES Authors(author_id)
+)
+
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215),
+
+)
+
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+)
+
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    customer_id REFERENCES Customers(customer_id)
+)
+
+CREATE TABLE Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    order_id REFERENCES Orders(order_id), book_id REFERENCES Books(book_id)
+)
