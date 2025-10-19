236bfd163f23e5538674f2d7e5ac4a3d4e105e83
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-10-19 17:59:18 +0300

    Confirming  connection to my database

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,11 +4,11 @@ from mysql.connector import Error
 def create_database():
     try:
         mydb = mysql.connector .connect (
-    host = "localhost",
-    user = "root",
-    password = "@Fromh3v3n",
-    # database = "alx_book_store"
-    )
+            host = "localhost",
+            user = "root",
+            password = "@Fromh3v3n",
+            # database = "alx_book_store"
+            )
         if mydb.is_connected():
             cursor = mydb.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS  alx_book_store")
