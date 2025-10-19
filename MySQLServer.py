b1c62d81554eae02f7f3efbdc5f2e88b23dac974
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-10-19 17:56:00 +0300

    Successful connection to my database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,28 @@
 import mysql.connector 
+from mysql.connector import Error
 
-mydb = mysql.connector .connect (
+def create_database():
+    try:
+        mydb = mysql.connector .connect (
     host = "localhost",
     user = "root",
     password = "@Fromh3v3n",
-    database = "alx_book_store"
-)
+    # database = "alx_book_store"
+    )
+        if mydb.is_connected():
+            cursor = mydb.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS  alx_book_store")
+            print(f"Database 'alx_book_store' created successfully!")
 
-if mydb.is_connected():
-    print(f"Database '{mydb.database}' created successfully!")
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
+    finally:
+        if mydb.is_connected():
+            cursor.close()
+            mydb.close()
+            
+
+
+if __name__ == "__main__":
+    create_database()
